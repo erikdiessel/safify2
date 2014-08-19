0162557d4613c38d737f69e8729ec7317e45fb7d
erikdiessel <erik.diessel@gmx.de>
2014-08-19 21:37:43 +0000

    Integrated requirejs into the testing infrastructure

---
+++ spec/mediatorSpec.js
@@ -0,0 +1,40 @@
+
+
+describe("Mediator", function() {
+	it("notifies other components about the event", function() {
+    	var handleCatastrope = jasmine.createSpy();
+    	md().on('catastrophe', handleCatastrope);
+        // would normally be done in another component:
+        md().pub('catastrophe');
+        expect(handleCatastrope).toHaveBeenCalled();
+    });
+    
+    it("passes along the additional information", function() {
+    	var defend = jasmine.createSpy();
+        md().on('attacked', defend);
+        
+        var place = "Epsilon Pegasi";
+        md().pub('attacked', place);
+        expect(defend).toHaveBeenCalledWith(place);
+    });
+    
+    it("works with multiple subscribers", function() {
+    	var callPolice = jasmine.createSpy();
+        var callAmbulance = jasmine.createSpy();
+        md().on('emergency', callPolice);
+        md().on('emergency', callAmbulance);
+        
+        md().pub('emergency');
+        expect(callPolice).toHaveBeenCalled();
+        expect(callAmbulance).toHaveBeenCalled();
+    });
+    
+    it("has the shortcut *publishing*", function() {
+    	var loadWeapons = jasmine.createSpy();
+        md().on('attacked', md().publishing('defending'));
+        md().on('defending', loadWeapons);
+        
+        md().pub('attacked');
+        expect(loadWeapons).toHaveBeenCalled();
+    });
+});
